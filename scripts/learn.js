1931da497725d0c8c9a7ffe41d88cf64991d74c3
Martyna Nowak <m.m.nowak187@gmail.com>
2023-08-03 22:37:46 +0000

    created a flipCard function in learn.js

--- scripts/learn.js
+++ scripts/learn.js
@@ -1,28 +1,28 @@
-const cardMeanings = document.getElementById("learn-card");
+const cardMeanings = document.getElementById("card");
 
 let cards = [
-    '<img src="assets/images/tarot/fool.jpg" alt="The Fool"></img>',
-    '<img src="assets/images/tarot/magician.jpg" alt="The Magician"></img>',
-    '<img src="assets/images/tarot/priestess.jpg" alt="The High Priestess"></img>',
-    '<img src="assets/images/tarot/empress.jpg" alt="The Empress"></img>',
-    '<img src="assets/images/tarot/emperor.jpg" alt="The Emperor"></img>',
-    '<img src="assets/images/tarot/hierophant.jpg" alt="The Hierophant"></img>',
-    '<img src="assets/images/tarot/lovers.jpg" alt="The Lovers"></img>',
-    '<img src="assets/images/tarot/chariot.jpg" alt="The Chariot"></img>',
-    '<img src="assets/images/tarot/strenghth.jpg" alt="Strength"></img>',
-    '<img src="assets/images/tarot/hermit.jpg" alt="The Hermit"></img>',
-    '<img src="assets/images/tarot/wheel.jpg" alt="The Wheel of Fortune"></img>',
-    '<img src="assets/images/tarot/justice.jpg" alt="Justice"></img>',
-    '<img src="assets/images/tarot/hanged.jpg" alt="The Hanged Man"></img>',
-    '<img src="assets/images/tarot/death.jpg" alt="Death"></img>',
-    '<img src="assets/images/tarot/temperance.jpg" alt="Temperance"></img>',
-    '<img src="assets/images/tarot/devil.jpg" alt="The Devil"></img>',
-    '<img src="assets/images/tarot/tower.jpg" alt="The Tower"></img>',
-    '<img src="assets/images/tarot/star.jpg" alt="The Star"></img>',
-    '<img src="assets/images/tarot/moon.jpg" alt="The Moon"></img>',
-    '<img src="assets/images/tarot/sun.jpg" alt="The Sun"></img>',
-    '<img src="assets/images/tarot/judgement.jpg" alt="Judgement"></img>',
-    '<img src="assets/images/tarot/world.jpg" alt="The World"></img>',
+    '<div class="card-front"><img src="assets/images/tarot/fool.jpg" alt="The Fool"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/magician.jpg" alt="The Magician"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/priestess.jpg" alt="The High Priestess"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/empress.jpg" alt="The Empress"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/emperor.jpg" alt="The Emperor"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/hierophant.jpg" alt="The Hierophant"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/lovers.jpg" alt="The Lovers"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/chariot.jpg" alt="The Chariot"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/strenghth.jpg" alt="Strength"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/hermit.jpg" alt="The Hermit"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/wheel.jpg" alt="The Wheel of Fortune"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/justice.jpg" alt="Justice"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/hanged.jpg" alt="The Hanged Man"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/death.jpg" alt="Death"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/temperance.jpg" alt="Temperance"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/devil.jpg" alt="The Devil"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/tower.jpg" alt="The Tower"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/star.jpg" alt="The Star"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/moon.jpg" alt="The Moon"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/sun.jpg" alt="The Sun"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/judgement.jpg" alt="Judgement"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/world.jpg" alt="The World"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit</p></div>',
 ];
 
 function showCard() {
@@ -30,3 +30,9 @@ function showCard() {
     currentCard = cards[cardIndex];
     cardMeanings.innerHTML = currentCard;
 };
+
+function flipCard() {
+    cardMeanings.classList.toggle('flip');
+}
+
+cardMeanings.addEventListener('click', flipCard);
