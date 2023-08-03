4fa2fbd9cc6dde7ae3bf2067fea1c803b722ce4f
Martyna Nowak <m.m.nowak187@gmail.com>
2023-08-03 15:48:07 +0000

    created a startGame and getNewCard function

--- scripts/script.js
+++ scripts/script.js
@@ -1,216 +1,229 @@
-const question = document.getElementById("card-question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const question = document.getElementById('card-question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 console.log(choices);
+console.log(question);
 
 let currentQuestion = {};
-let acceptingAnswer = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let avaliableQuestions = [];
+let availableQuestions = [];
 
 let questions = [
     {
         question: '<img src="assets/images/tarot/fool.jpg" alt="The Fool"></img>',
-        choiceA: "Good luck, karma, life cycles, destiny, a turning point",
-        choiceB: "Hope, faith, purpose, renewal, spirituality",
-        choiceC: "Beginnings, innocence, spontaneity, a free spirit",
-        choiceD: "Completion, integration, accomplishment, travel",
+        choice1: 'Good luck, karma, life cycles, destiny, a turning point',
+        choice2: 'Hope, faith, purpose, renewal, spirituality',
+        choice3: 'Beginnings, innocence, spontaneity, a free spirit',
+        choice4: 'Completion, integration, accomplishment, travel',
         answer: 3
     },
 
     {
         question: '<img src="assets/images/tarot/magician.jpg" alt="The Magician"></img>',
-        choiceA: "Manifestation, resourcefulness, power, inspired action",
-        choiceB: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
-        choiceC: "Shadow self, attachment, addiction, restriction, sexuality",
-        choiceD: "Endings, change, transformation, transition",
+        choice1: 'Manifestation, resourcefulness, power, inspired action',
+        choice2: 'Spiritual wisdom, religious beliefs, conformity, tradition, institutions',
+        choice3: 'Shadow self, attachment, addiction, restriction, sexuality',
+        choice4: 'Endings, change, transformation, transition',
         answer: 1
     },
 
     {
         question: '<img src="assets/images/tarot/priestess.jpg" alt="The High Priestess"></img>',
-        choiceA: "Femininity, beauty, nature, nurturing, abundance",
-        choiceB: "Intuition, sacred knowledge, divine feminine, the subconscious mind",
-        choiceC: "Soul-searching, introspection, being alone, inner guidance",
-        choiceD: "Positivity, fun, warmth, success, vitality",
+        choice1: 'Femininity, beauty, nature, nurturing, abundance',
+        choice2: 'Intuition, sacred knowledge, divine feminine, the subconscious mind',
+        choice3: 'Soul-searching, introspection, being alone, inner guidance',
+        choice4: 'Positivity, fun, warmth, success, vitality',
         answer: 2
     },
-
+    
     {
         question: '<img src="assets/images/tarot/empress.jpg" alt="The Empress"></img>',
-        choiceA: "Soul-searching, introspection, being alone, inner guidance",
-        choiceB: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
-        choiceC: "Femininity, beauty, nature, nurturing, abundance",
-        choiceD: "Illusion, fear, anxiety, subconscious, intuition",
+        choice1: "Soul-searching, introspection, being alone, inner guidance",
+        choice2: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
+        choice3: "Femininity, beauty, nature, nurturing, abundance",
+        choice4: "Illusion, fear, anxiety, subconscious, intuition",
         answer: 3
     },
 
     {
         question: '<img src="assets/images/tarot/emperor.jpg" alt="The Emperor"></img>',
-        choiceA: "Authority, establishment, structure, a father figure",
-        choiceB: "Control, willpower, success, action, determination",
-        choiceC: "Fairness, truth, cause and effect, law",
-        choiceD: "Manifestation, resourcefulness, power, inspired action",
+        choice1: "Authority, establishment, structure, a father figure",
+        choice2: "Control, willpower, success, action, determination",
+        choice3: "Fairness, truth, cause and effect, law",
+        choice4: "Manifestation, resourcefulness, power, inspired action",
         answer: 1
     },
 
     {
         question: '<img src="assets/images/tarot/hierophant.jpg" alt="The Hierophant"></img>',
-        choiceA: "Soul-searching, introspection, being alone, inner guidance",
-        choiceB: "Hope, faith, purpose, renewal, spirituality",
-        choiceC: "Completion, integration, accomplishment, travel",
-        choiceD: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
+        choice1: "Soul-searching, introspection, being alone, inner guidance",
+        choice2: "Hope, faith, purpose, renewal, spirituality",
+        choice3: "Completion, integration, accomplishment, travel",
+        choice4: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
         answer: 4
     },
 
     {
         question: '<img src="assets/images/tarot/lovers.jpg" alt="The Lovers"></img>',
-        choiceA: "Femininity, beauty, nature, nurturing, abundance",
-        choiceB: "Positivity, fun, warmth, success, vitality",
-        choiceC: "Love, harmony, relationships, values alignment, choices",
-        choiceD: "Balance, moderation, patience, purpose",
+        choice1: "Femininity, beauty, nature, nurturing, abundance",
+        choice2: "Positivity, fun, warmth, success, vitality",
+        choice3: "Love, harmony, relationships, values alignment, choices",
+        choice4: "Balance, moderation, patience, purpose",
         answer: 3
     },
 
     {
         question: '<img src="assets/images/tarot/chariot.jpg" alt="The Chariot"></img>',
-        choiceA: "Fairness, truth, cause and effect, law",
-        choiceB: "Hope, faith, purpose, renewal, spirituality",
-        choiceC: "Balance, moderation, patience, purpose",
-        choiceD: "Control, willpower, success, action, determination",
+        choice1: "Fairness, truth, cause and effect, law",
+        choice2: "Hope, faith, purpose, renewal, spirituality",
+        choice3: "Balance, moderation, patience, purpose",
+        choice4: "Control, willpower, success, action, determination",
         answer: 4
     },
 
     {
         question: '<img src="assets/images/tarot/strength.jpg" alt="Strength"></img>',
-        choiceA: "Completion, integration, accomplishment, travel",
-        choiceB: "Courage, persuasion, influence, compassion",
-        choiceC: "Control, willpower, success, action, determination",
-        choiceD: "Fairness, truth, cause and effect, law",
+        choice1: "Completion, integration, accomplishment, travel",
+        choice2: "Courage, persuasion, influence, compassion",
+        choice3: "Control, willpower, success, action, determination",
+        choice4: "Fairness, truth, cause and effect, law",
         answer: 2
     },
 
     {
         question: '<img src="assets/images/tarot/hermit.jpg" alt="The Hermit"></img>',
-        choiceA: "Endings, change, transformation, transition",
-        choiceB: "Illusion, fear, anxiety, subconscious, intuition",
-        choiceC: "Soul-searching, introspection, being alone, inner guidance",
-        choiceD: "Intuition, sacred knowledge, divine feminine, the subconscious mind",
+        choice1: "Endings, change, transformation, transition",
+        choice2: "Illusion, fear, anxiety, subconscious, intuition",
+        choice3: "Soul-searching, introspection, being alone, inner guidance",
+        choice4: "Intuition, sacred knowledge, divine feminine, the subconscious mind",
         answer: 3
     },
 
     {
         question: '<img src="assets/images/tarot/wheel.jpg" alt="The Wheel of Fortune"></img>',
-        choiceA: "Completion, integration, accomplishment, travel",
-        choiceB: "Endings, change, transformation, transition",
-        choiceC: "Courage, persuasion, influence, compassion",
-        choiceD: "Good luck, karma, life cycles, destiny, a turning point",
+        choice1: "Completion, integration, accomplishment, travel",
+        choice2: "Endings, change, transformation, transition",
+        choice3: "Courage, persuasion, influence, compassion",
+        choice4: "Good luck, karma, life cycles, destiny, a turning point",
         answer: 4
     },
 
     {
         question: '<img src="assets/images/tarot/justice.jpg" alt="Justice"></img>',
-        choiceA: "Authority, establishment, structure, a father figure",
-        choiceB: "Balance, moderation, patience, purpose",
-        choiceC: "Fairness, truth, cause and effect, law",
-        choiceD: "Shadow self, attachment, addiction, restriction, sexuality",
+        choice1: "Authority, establishment, structure, a father figure",
+        choice2: "Balance, moderation, patience, purpose",
+        choice3: "Fairness, truth, cause and effect, law",
+        choice4: "Shadow self, attachment, addiction, restriction, sexuality",
         answer: 3
     },
 
     {
         question: '<img src="assets/images/tarot/hanged.jpg" alt="The Hanged Man"></img>',
-        choiceA: "Endings, change, transformation, transition",
-        choiceB: "Pause, surrender, letting go, new perspectives",
-        choiceC: "Illusion, fear, anxiety, subconscious, intuition",
-        choiceD: "Soul-searching, introspection, being alone, inner guidance",
+        choice1: "Endings, change, transformation, transition",
+        choice2: "Pause, surrender, letting go, new perspectives",
+        choice3: "Illusion, fear, anxiety, subconscious, intuition",
+        choice4: "Soul-searching, introspection, being alone, inner guidance",
         answer: 2
     },
 
     {
         question: '<img src="assets/images/tarot/death.jpg" alt="Death"></img>',
-        choiceA: "Sudden change, upheaval, chaos, revelation, awakening",
-        choiceB: "Shadow self, attachment, addiction, restriction, sexuality",
-        choiceC: "Rebirth, inner calling, absolution",
-        choiceD: "Endings, change, transformation, transition",
+        choice1: "Sudden change, upheaval, chaos, revelation, awakening",
+        choice2: "Shadow self, attachment, addiction, restriction, sexuality",
+        choice3: "Rebirth, inner calling, absolution",
+        choice4: "Endings, change, transformation, transition",
         answer: 4
     },
 
     {
         question: '<img src="assets/images/tarot/temperance.jpg" alt="Temperance"></img>',
-        choiceA: "Control, willpower, success, action, determination",
-        choiceB: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
-        choiceC: "Balance, moderation, patience, purpose",
-        choiceD: "Pause, surrender, letting go, new perspectives",
+        choice1: "Control, willpower, success, action, determination",
+        choice2: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
+        choice3: "Balance, moderation, patience, purpose",
+        choice4: "Pause, surrender, letting go, new perspectives",
         answer: 3
     },
 
     {
         question: '<img src="assets/images/tarot/devil.jpg" alt="The Devil"></img>',
-        choiceA: "Endings, change, transformation, transition",
-        choiceB: "Shadow self, attachment, addiction, restriction, sexuality",
-        choiceC: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
-        choiceD: "Intuition, sacred knowledge, divine feminine, the subconscious mind",
+        choice1: "Endings, change, transformation, transition",
+        choice2: "Shadow self, attachment, addiction, restriction, sexuality",
+        choice3: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
+        choice4: "Intuition, sacred knowledge, divine feminine, the subconscious mind",
         answer: 2
     },
 
     {
         question: '<img src="assets/images/tarot/tower.jpg" alt="The Tower"></img>',
-        choiceA: "Sudden change, upheaval, chaos, revelation, awakening",
-        choiceB: "Rebirth, inner calling, absolution",
-        choiceC: "Illusion, fear, anxiety, subconscious, intuition",
-        choiceD: "Fairness, truth, cause and effect, law",
+        choice1: "Sudden change, upheaval, chaos, revelation, awakening",
+        choice2: "Rebirth, inner calling, absolution",
+        choice3: "Illusion, fear, anxiety, subconscious, intuition",
+        choice4: "Fairness, truth, cause and effect, law",
         answer: 1
     },
 
     {
         question: '<img src="assets/images/tarot/star.jpg" alt="The Star"></img>',
-        choiceA: "Beginnings, innocence, spontaneity, a free spirit",
-        choiceB: "Positivity, fun, warmth, success, vitality",
-        choiceC: "Good luck, karma, life cycles, destiny, a turning point",
-        choiceD: "Hope, faith, purpose, renewal, spirituality",
+        choice1: "Beginnings, innocence, spontaneity, a free spirit",
+        choice2: "Positivity, fun, warmth, success, vitality",
+        choice3: "Good luck, karma, life cycles, destiny, a turning point",
+        choice4: "Hope, faith, purpose, renewal, spirituality",
         answer: 4
     },
 
     {
         question: '<img src="assets/images/tarot/moon.jpg" alt="The Moon"></img>',
-        choiceA: "Illusion, fear, anxiety, subconscious, intuition",
-        choiceB: "Love, harmony, relationships, values alignment, choices",
-        choiceC: "Intuition, sacred knowledge, divine feminine, the subconscious mind",
-        choiceD: "Femininity, beauty, nature, nurturing, abundance",
+        choice1: "Illusion, fear, anxiety, subconscious, intuition",
+        choice2: "Love, harmony, relationships, values alignment, choices",
+        choice3: "Intuition, sacred knowledge, divine feminine, the subconscious mind",
+        choice4: "Femininity, beauty, nature, nurturing, abundance",
         answer: 1
     },
 
     {
         question: '<img src="assets/images/tarot/sun.jpg" alt="The Sun"></img>',
-        choiceA: "Good luck, karma, life cycles, destiny, a turning point",
-        choiceB: "Positivity, fun, warmth, success, vitality",
-        choiceC: "Beginnings, innocence, spontaneity, a free spirit",
-        choiceD: "Authority, establishment, structure, a father figure",
+        choice1: "Good luck, karma, life cycles, destiny, a turning point",
+        choice2: "Positivity, fun, warmth, success, vitality",
+        choice3: "Beginnings, innocence, spontaneity, a free spirit",
+        choice4: "Authority, establishment, structure, a father figure",
         answer: 2
     },
 
     {
         question: '<img src="assets/images/tarot/judgement.jpg" alt="Judgement"></img>',
-        choiceA: "Rebirth, inner calling, absolution",
-        choiceB: "Completion, integration, accomplishment, travel",
-        choiceC: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
-        choiceD: "Fairness, truth, cause and effect, law",
+        choice1: "Rebirth, inner calling, absolution",
+        choice2: "Completion, integration, accomplishment, travel",
+        choice3: "Spiritual wisdom, religious beliefs, conformity, tradition, institutions",
+        choice4: "Fairness, truth, cause and effect, law",
         answer: 1
     },
 
     {
         question: '<img src="assets/images/tarot/world.jpg" alt="The World"></img>',
-        choiceA: "Good luck, karma, life cycles, destiny, a turning point",
-        choiceB: "Manifestation, resourcefulness, power, inspired action",
-        choiceC: "Sudden change, upheaval, chaos, revelation, awakening",
-        choiceD: "Completion, integration, accomplishment, travel",
+        choice1: "Good luck, karma, life cycles, destiny, a turning point",
+        choice2: "Manifestation, resourcefulness, power, inspired action",
+        choice3: "Sudden change, upheaval, chaos, revelation, awakening",
+        choice4: "Completion, integration, accomplishment, travel",
         answer: 4
     },
 ];
 
  function startGame() {
-    avaliableQuestions = [...questions];
-    console.log(avaliableQuestions);
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewCard();
+ }
+
+ function getNewCard() {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex]
+    console.log(currentQuestion);
+    question.innerHTML = currentQuestion.question;
  }
 
  startGame();
+
