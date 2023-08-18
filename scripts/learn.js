b6f65b2fc79670214f787b41dd0281bd1c809740
Martyna Nowak <m.m.nowak187@gmail.com>
2023-08-18 20:32:07 +0000

    the modal on the learn page now displays correct value obtained from the tarot_api

--- scripts/learn.js
+++ scripts/learn.js
@@ -6,50 +6,29 @@ const cardMeanings = document.getElementById("card");
  * The meaning will be revealed when the card is clicked.
  */
 var cards = [
-    '<div class="card-front"><img id="fool" src="assets/images/tarot/fool.jpg" alt="The Fool"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit<button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/magician.jpg" alt="The Magician"></img></div><div class="card-reverse"><p>Manifestation, resourcefulness, power, inspired action <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/priestess.jpg" alt="The High Priestess"></img></div><div class="card-reverse"><p>Intuition, sacred knowledge, divine feminine, the subconscious mind <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/empress.jpg" alt="The Empress"></img></div><div class="card-reverse"><p>Femininity, beauty, nature, nurturing, abundance <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/emperor.jpg" alt="The Emperor"></img></div><div class="card-reverse"><p>Authority, establishment, structure, a father figure <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/hierophant.jpg" alt="The Hierophant"></img></div><div class="card-reverse"><p>Spiritual wisdom, religious beliefs, conformity, tradition, institutions <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/lovers.jpg" alt="The Lovers"></img></div><div class="card-reverse"><p>Love, harmony, relationships, values alignment, choices <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/chariot.jpg" alt="The Chariot"></img></div><div class="card-reverse"><p>Control, willpower, success, action, determination <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/strenghth.jpg" alt="Strength"></img></div><div class="card-reverse"><p>Strength, courage, persuasion, influence, compassion <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/hermit.jpg" alt="The Hermit"></img></div><div class="card-reverse"><p>Soul-searching, introspection, being alone, inner guidance <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/wheel.jpg" alt="The Wheel of Fortune"></img></div><div class="card-reverse"><p>Good luck, karma, life cycles, destiny, a turning point <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/justice.jpg" alt="Justice"></img></div><div class="card-reverse"><p>Justice, fairness, truth, cause and effect, law <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/hanged.jpg" alt="The Hanged Man"></img></div><div class="card-reverse"><p>Pause, surrender, letting go, new perspectives <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/death.jpg" alt="Death"></img></div><div class="card-reverse"><p>Endings, change, transformation, transition <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/temperance.jpg" alt="Temperance"></img></div><div class="card-reverse"><p>Balance, moderation, patience, purpose <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/devil.jpg" alt="The Devil"></img></div><div class="card-reverse"><p>Shadow self, attachment, addiction, restriction, sexuality <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/tower.jpg" alt="The Tower"></img></div><div class="card-reverse"><p>Sudden change, upheaval, chaos, revelation, awakening <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/star.jpg" alt="The Star"></img></div><div class="card-reverse"><p>Hope, faith, purpose, renewal, spirituality <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/moon.jpg" alt="The Moon"></img></div><div class="card-reverse"><p>Illusion, fear, anxiety, subconscious, intuition <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/sun.jpg" alt="The Sun"></img></div><div class="card-reverse"><p>Positivity, fun, warmth, success, vitality <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/judgement.jpg" alt="Judgement"></img></div><div class="card-reverse"><p>Judgement, rebirth, inner calling, absolution <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
-    '<div class="card-front"><img src="assets/images/tarot/world.jpg" alt="The World"></img></div><div class="card-reverse"><p>Completion, integration, accomplishment, travel <button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">See more</button></p></div>',
+    '<div class="card-front"><img id="fool" src="assets/images/tarot/fool.jpg" alt="The Fool"></img></div><div class="card-reverse"><p>Beginnings, innocence, spontaneity, a free spirit<br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">See more</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/magician.jpg" alt="The Magician"></img></div><div class="card-reverse"><p>Manifestation, resourcefulness, power, inspired action <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/empress.jpg" alt="The Empress"></img></div><div class="card-reverse"><p>Femininity, beauty, nature, nurturing, abundance <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/emperor.jpg" alt="The Emperor"></img></div><div class="card-reverse"><p>Authority, establishment, structure, a father figure <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/priestess.jpg" alt="The High Priestess"></img></div><div class="card-reverse"><p>Intuition, sacred knowledge, divine feminine, the subconscious mind <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/hierophant.jpg" alt="The Hierophant"></img></div><div class="card-reverse"><p>Spiritual wisdom, religious beliefs, conformity, tradition, institutions <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/lovers.jpg" alt="The Lovers"></img></div><div class="card-reverse"><p>Love, harmony, relationships, values alignment, choices <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/chariot.jpg" alt="The Chariot"></img></div><div class="card-reverse"><p>Control, willpower, success, action, determination <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/justice.jpg" alt="Justice"></img></div><div class="card-reverse"><p>Justice, fairness, truth, cause and effect, law <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/temperance.jpg" alt="Temperance"></img></div><div class="card-reverse"><p>Balance, moderation, patience, purpose <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/strenghth.jpg" alt="Strength"></img></div><div class="card-reverse"><p>Strength, courage, persuasion, influence, compassion <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/hermit.jpg" alt="The Hermit"></img></div><div class="card-reverse"><p>Soul-searching, introspection, being alone, inner guidance <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/wheel.jpg" alt="The Wheel of Fortune"></img></div><div class="card-reverse"><p>Good luck, karma, life cycles, destiny, a turning point <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/hanged.jpg" alt="The Hanged Man"></img></div><div class="card-reverse"><p>Pause, surrender, letting go, new perspectives <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/death.jpg" alt="Death"></img></div><div class="card-reverse"><p>Endings, change, transformation, transition <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/devil.jpg" alt="The Devil"></img></div><div class="card-reverse"><p>Shadow self, attachment, addiction, restriction, sexuality <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/tower.jpg" alt="The Tower"></img></div><div class="card-reverse"><p>Sudden change, upheaval, chaos, revelation, awakening <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/star.jpg" alt="The Star"></img></div><div class="card-reverse"><p>Hope, faith, purpose, renewal, spirituality <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/moon.jpg" alt="The Moon"></img></div><div class="card-reverse"><p>Illusion, fear, anxiety, subconscious, intuition <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/sun.jpg" alt="The Sun"></img></div><div class="card-reverse"><p>Positivity, fun, warmth, success, vitality <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/judgement.jpg" alt="Judgement"></img></div><div class="card-reverse"><p>Judgement, rebirth, inner calling, absolution <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
+    '<div class="card-front"><img src="assets/images/tarot/world.jpg" alt="The World"></img></div><div class="card-reverse"><p>Completion, integration, accomplishment, travel <br><button type="button" class="btn meaning-btn" data-bs-toggle="modal" data-bs-target="#fullmeaningmodal">Full Meaning</button></p></div>',
 ];
-
-// Loads the first card straight away
-window.onload = showCard();
-
-// Loads a tarot card
-function showCard() {
-    cardMeanings.classList.remove('flip');
-    const cardIndex = Math.floor(Math.random() * cards.length);
-    currentCard = cards[cardIndex];
-    cardMeanings.innerHTML = currentCard;
-    cards.splice(cardIndex, 1); // Allows showing a card only once per deck
-    if (cards.length === 0) {window.location.reload()}; // Start showing the cards again
-};
-
-// Shows a card meaning
-function flipCard() {
-    cardMeanings.classList.toggle('flip');
-};
-
-cardMeanings.addEventListener('click', flipCard);
-
 
 // Gets data from tarot_api by howlCode
 
@@ -68,12 +47,33 @@ function getData(cb) {
     };
 };
 
-// Shows the full meaning of a card
+// Loads a tarot card
+function showCard() {
+    cardMeanings.classList.remove('flip');
+    cardIndex = Math.floor(Math.random() * cards.length);
+    currentCard = cards[cardIndex];
+    cardMeanings.innerHTML = currentCard;
+
+// Shows the full meaning of a card retreived from tarot_api (see more in the README file)
 function showFullMeaning(obj) {
-    cards.forEach(function(card) {
-    var indNum = cards.indexOf(card);
-    document.getElementById("fullmeaning").innerText = obj[indNum].full_meaning
+    console.log(currentCard);
+    console.log(cardIndex);
+    cards.forEach(function callback(card, index) {
+    document.getElementById("fullmeaning").innerText = obj[cardIndex].full_meaning
 });
 };
 
 getData(showFullMeaning);
+
+};
+
+// Shows a card meaning
+function flipCard() {
+    cardMeanings.classList.toggle('flip');
+};
+
+cardMeanings.addEventListener('click', flipCard);
+
+// Loads the first card straight away
+window.onload = showCard();
+
