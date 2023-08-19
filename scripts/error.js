8a66dedc7e19d7606414e4aa8b57219a4a305fd1
Martyna Nowak <m.m.nowak187@gmail.com>
2023-08-19 00:59:43 +0000

    added meta description and tags

--- scripts/error.js
+++ scripts/error.js
@@ -1,10 +1,8 @@
  // Redirects to the index page after 10 seconds when error 404 is encountered
- if (url.statusCode === '404') {
-    setTimeout(function () {
+setTimeout(function () {
       window.location.href = './index.html';
-    }, 10000);    
+}, 10000);    
 
-};
 // Countdown timer showing when the user will be redirected to the index page when error 404 is encountered
 
 var timeLeft = 9;
