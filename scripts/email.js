abc5077bf782cf520466e849d0eab5be3d9c6c4b
Martyna Nowak <m.m.nowak187@gmail.com>
2023-08-24 12:55:13 +0000

    fixed the sendMail function

--- scripts/email.js
+++ scripts/email.js
@@ -2,9 +2,11 @@
 
 // EmailJS API implemented with a tutorial from CodeInstitute and EmailJS documentation
 
-emailjs.sendForm("service_x7xdzzi", "tarotquizemail", "#contact-form")
+function sendMail(contactForm) {
+
+emailjs.send("service_x7xdzzi", "tarotquizemail")
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
     }, function(error) {
        console.log('FAILED...', error);
-    });
+    });}
