dc3babd2afebe610b0afbf949be5569e2ae53d0c
Martyna Nowak <m.m.nowak187@gmail.com>
2023-08-24 18:49:49 +0000

    tidied up the code

--- scripts/email.js
+++ scripts/email.js
@@ -1,12 +1,13 @@
 /*jshint esversion: 6 */
+
 const thankYouModal = document.getElementById("thankyou-modal");
-// EmailJS API implemented with a tutorial from CodeInstitute and EmailJS documentation
 
 // Displays a Bootstrap modal thanking the user for their message
 function thankYouMessage() {
    $(thankYouModal).modal('show');
 }
 
+// EmailJS API implemented with a tutorial from CodeInstitute and EmailJS documentation
 function sendMail(contactForm) {
    emailjs.send("service_x7xdzzi", "tarotquizemail") // Service ID, Template ID
     .then(function(response) {
